ec70640a7b4186de737c45985a13b5948556409e
JakeD <jakediefenderfer@gmail.com>
2022-07-18 21:54:45 -0400

    stub up potential match card

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,6 +1,13 @@
+import * as React from 'react';
 import { useState, useEffect } from 'react'
 import * as profileService from '../../services/profileService'
-
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Logo from '../../assets/zombieapp-logo.png'
 
 const Profiles = () => {
   const [profiles, setProfiles] = useState([])
@@ -14,54 +21,77 @@ const Profiles = () => {
   }, [])
 
   return (
-    <>
-      <h1>Potential Matches</h1>
-      {profiles.length ? 
-        <>
-          {profiles.map(profile =>
-            <div key={profile._id}>
-              <h1>
-              {profile.name}
-              </h1> 
-              <p> Species: 
-              {profile.species}
-              </p>
-              <p> Do you eat brains?
-              {profile.brains = true ? "yes, I eat brains" : "do not prefer to eat brains"}
-              </p>
-              <p> Prefer to date: 
-              {profile.prefersZombie = true ? "Zombies" : profile.prefersHalfbie = true ? "Halbies": profile.prefersHuman = true ? "Humans" : "I ain't got no type"}
-              </p>
-              <p>
-              {profile.prefersHuman}
+    <Card sx={{ maxWidth: 345, marginTop: "200px" }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={Logo}
+        alt=""
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Name
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+          Age
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Bio
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    // <div>
+    //   <h1>Potential Matches</h1>
+    //   {profiles.length ? 
+    //     <>
+    //       {profiles.map(profile =>
+    //         <div key={profile._id}>
+    //           <h1>
+    //           {profile.name}
+    //           </h1> 
+    //           <p> Species: 
+    //           {profile.species}
+    //           </p>
+    //           <p> Do you eat brains?
+    //           {profile.brains = true ? "yes, I eat brains" : "do not prefer to eat brains"}
+    //           </p>
+    //           <p> Prefer to date: 
+    //           {profile.prefersZombie = true ? "Zombies" : profile.prefersHalfbie = true ? "Halbies": profile.prefersHuman = true ? "Humans" : "I ain't got no type"}
+    //           </p>
+    //           <p>
+    //           {profile.prefersHuman}
 
-              </p>
-              <p>
-              {profile.prefersHalfbie}
-              </p>
-              <p>
-              {profile.age} years old
+    //           </p>
+    //           <p>
+    //           {profile.prefersHalfbie}
+    //           </p>
+    //           <p>
+    //           {profile.age} years old
 
-              </p>
-              height:
-              <p>
-              {profile.height}"
+    //           </p>
+    //           height:
+    //           <p>
+    //           {profile.height}"
 
-              </p>
-              About Me: 
-              <p>
-              {profile.bio}
-              </p>
+    //           </p>
+    //           About Me: 
+    //           <p>
+    //           {profile.bio}
+    //           </p>
 
-              </div>
+    //           </div>
 
 
-          )}
-        </>
-      :
-        <p>No profiles yet</p>
-      }
-    </>
+    //       )}
+    //     </>
+    //   :
+    //     <p>No profiles yet</p>
+    //   }
+    // </div>
   )
 }
 
